1d727fcb80c99498343e75ebd8eb3a6aaee0c666
Arun Zechariah <arun@zechariah.net>
2017-03-22 17:12:06 +0530

    added code to prefix/suffix randomized number

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.get('/:asset/by/:param/:value', function (req, res) {
   //var barc_test = '{"text": "12345", "format":"EAN-13", "cancelled": false}';
   var barcTest = {}
   barcTest.deviceNumber = req.params.value;
-  barcTest.serialNumber = '13AN';
-  barcTest.lotNumber = '231ZT'
+  barcTest.serialNumber = Math.floor((Math.random() * 1000) + 1) + 'AN';
+  barcTest.lotNumber = Math.floor((Math.random() * 100) + 1) + 'ZT' + Math.floor((Math.random() * 1000) + 1);
   res.status(200).json(barcTest);
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3017, function () {
+  console.log('Example app listening on port 3017!');
 })
