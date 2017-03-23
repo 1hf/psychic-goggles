7ad441557eb3f8baff6ce882a81cc444102b5acc
Arun Zechariah <arun@zechariah.net>
2017-03-23 15:39:27 +0530

    assets by hospitalId

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Test barcode API');
+app.get('/', function(req, res) {
+	res.send('Test barcode API');
 })
 
-app.get('/:asset/by/:param/:value', function (req, res) {
-  //console.log(req.params);
-  //var barc_test = '{"text": "12345", "format":"EAN-13", "cancelled": false}';
-  var barcTest = {}
-  //barcTest.deviceNumber = req.params.value;
+app.get('/:asset/by/:param/:value', function(req, res) {
+	//console.log(req.params);
+	//var barc_test = '{"text": "12345", "format":"EAN-13", "cancelled": false}';
+	var barcTest = {};
+	//barcTest.deviceNumber = req.params.value;
 
-for(var i = 0; i < deviceSensors.length; i++)
-{
-  if(deviceSensors[i].DeviceSensorID == req.params.value)
-  {
-    barcTest = deviceSensors[i];
-    //console.log(deviceSensors[i]);
-    res.status(200).json(barcTest);
-  }
-}
+	var retData = {};
+
+	if (req.params.param == 'barcode') {
+		for (var i = 0; i < deviceSensors.length; i++) {
+			if (deviceSensors[i].DeviceSensorID == req.params.value) {
+				barcTest = deviceSensors[i];
+				//console.log(deviceSensors[i]);
+				res.status(200).json(barcTest);
+			}
+		}
+	}
+
+	if (req.params.param == 'hospitalId') {
+		retData.devices = [{
+			deviceNumber: 3660,
+			serialNumber: 141515,
+			installDate: "03-Feb-2017"
+		},
+		{
+			deviceNumber: 3660,
+			serialNumber: 141515,
+			installDate: "03-Feb-2017"			
+		}];
+		res.status(200).json(retData);
+	}
 
 })
 
-app.listen(3017, function () {
-  console.log('Example app listening on port 3017!');
+app.listen(3017, function() {
+	console.log('Example app listening on port 3017!');
 })
 
 var deviceSensors = require("./deviceSensors.json");
